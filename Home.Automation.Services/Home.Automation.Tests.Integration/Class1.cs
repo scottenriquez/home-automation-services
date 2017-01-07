ad346abd4509072f73c838a63840ac0830d1ce98
Scott Enriquez <scottie@enriquez.io>
2017-01-06 22:15:08 -0600

    I messed up the file paths again

---
+++ Home.Automation.Services/Home.Automation.Tests.Integration/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home.Automation.Tests.Integration
+{
+    public class Class1
+    {
+    }
+}
